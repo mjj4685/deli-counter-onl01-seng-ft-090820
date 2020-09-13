e7fe0186b34ac765b17396d319391a026e583f72
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 03:33:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,24 @@
-def take_a_number(line, patron)
-  line.push(patron)
-  puts "Welcome, #{patron}. You are number #{line.length} in line."
+def line(patron)
+  if patron.empy?
+    puts "The line is currently empty."
+  else line_currently = "This line is currently:"
+    patron.each.with_index(1) do |person,i|
+      line_currently<<"#{i}. #{person}"
+    end 
+    puts line_currently
+  end
 end
 
-take_a_number(katz_deli, "")
+def take_a_number(counter, name)
+  counter<<name
+  puts "Welcome, #{name}. You are number #{counter.length} in line."
+end
 
-def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
+def now_serving(patron)
+  if patron.empty?
+    puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}."
-    line.shift
+    puts "Currently serving #{patron.first}."
+    counter.shift
   end
 end

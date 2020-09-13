22641fe43c413ceff134b808049fb45567e2d7fd
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 00:26:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def take_a_number(array, name)
+  katz_deli.push(name)
+  counter = 1
+    katz_deli.each_with_index do |name, counter|
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    counter += 1
+  end
+
+def now_serving(katz_deli, name)
+    katz_deli << (name)
+    puts "Currently serving #{name}."
+end
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+ end
+
+ end

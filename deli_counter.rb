cbb40dd44752234c14f4d752cf10c3d7b27a6f65
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 00:40:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
-  puts "Currently serving #{katz_deli[0]}."
+  puts "Currently serving #{katz_deli}."
   katz_deli.shift
  end
 

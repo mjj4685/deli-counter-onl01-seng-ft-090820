931c7c27b20a236032745927c62790efd87b76f3
MJ Johnson <mjohnson4685@gmail.com>
2020-09-13 01:32:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,8 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-  puts "Currently serving #{katz_deli}.".each_with_index
-  katz_deli.shift
+  puts "Currently serving #{katz_deli}."
+  katz_deli.each_with_index
  end
 
  end
